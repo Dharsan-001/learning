33b5d9aadffbc95c1779005001a87f153fa1e10b
Dharsan-001 <82573154+Dharsan-001@users.noreply.github.com>
2022-09-21 11:12:11 +0530

    Add files via upload

---
+++ learning/src/learning/Learning.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learning;
+
+import java.util.*;
+
+/**
+ *
+ * @author HP
+ */
+public class Learning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = scan.nextInt();
+        System.out.println("The number is "+num);
+    }
+    
+}
